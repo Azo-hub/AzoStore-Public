dd2fc2cd42ae90d101bc8216af0fe64ae6d98b05
Azeez Ridwan <readone.cybernet@gmail.com>
2021-01-21 22:41:54 +0100

    Update AzoStoreApplication.java

--- AzoStore/src/main/java/AzoStore/AzoStoreApplication.java
+++ AzoStore/src/main/java/AzoStore/AzoStoreApplication.java
@@ -25,7 +25,7 @@ import AzoStore.ServicePackage.UserService;
 
 @SpringBootApplication
 @EnableScheduling
-public class AzoStoreApplication implements CommandLineRunner {
+public class AzoStoreApplication {
 	@Autowired
 	private UserService userService;
 	
@@ -50,23 +50,6 @@ public class AzoStoreApplication implements CommandLineRunner {
 	}
 	
 	
-	@Override
-	public void run(String... args) throws Exception {
-			
-			User user1 = new User();
-			user1.setFirstname("Ridwan");
-			user1.setLastname("Azeez");
-			user1.setUsername("r");
-			user1.setPassword(SecurityUtility.passwordEncoder().encode("r"));
-			user1.setEmail("readone.cybernet@gmail.com");
-			Set<UserRole> userRoles = new HashSet<>();
-			Role role1 = new Role();
-			role1.setRoleId(1);
-			role1.setName("ROLE_USER");
-			userRoles.add(new UserRole(user1, role1));
-			
-			userService.createUser(user1, userRoles);
-		}
 		
 
 	
